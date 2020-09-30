283a0ea87095898963f24c70de945d7b567cfba2
unknown <rajbipin1412@gmail.com>
2020-10-01 00:53:48 +0530

    PrimeNumber.cpp modified

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,42 +1,21 @@
-#include<iostream> // library for basic input/output functions
-#include<cmath> // library for using math functions
- 
+#include<bits/stdc++.h>
 using namespace std;
- 
-int main()
-{
-   int n, status = 1, num = 3, count, c;
-   //prompting user to enter number of prime numbers to print
-   cout << "Enter the number of prime numbers to print: ";
-   cin >> n;
- 
-   // input is valid display first prime number
-   if ( n >= 1 )
-   {
-      cout << "First " << n <<" prime numbers are :-" << endl;
-      cout << 2 << endl;
-   }
- 
-   for ( count = 2 ; count <=n ;  ) //loop that will iterate through n numbers
-   {
-      for ( c = 2 ; c <= (int)sqrt(num) ; c++ )// for each element check whether it is prime or not
-      {
-        //if number is completely divisible by a number other than 1 and itselft,then number is not prime
-         if ( num%c == 0 ) 
-         {
-            status = 0;
-            break;
-         }
-      }
-      //if it is a prime number, print it
-      if ( status != 0 )
-      {
-         cout << num << endl;
-         count++;
-      }
-      status = 1;
-      num++;
-   }         
- 
-   return 0;
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+		a[i] = 0;
+	for(int i=2;i<=sqrt(n);i++){
+		for(int j = i+1;j<=n;j++){
+			if(j%i == 0)
+				a[j] = 1;
+		}
+	}
+	for(int i=2;i<=n;i++){
+		if(a[i]==0)
+			cout<<i<<endl;
+	}
+	return 0;
 }
